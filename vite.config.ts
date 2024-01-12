218a31e1500f7c1a0c33d8841f89413977f29744
siegelioner <576075394@qq.com>
2024-01-12 16:49:45 +0800

    VueDevTools

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,39 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import VueDevTools from 'vite-plugin-vue-devtools'
 
+interface VitePluginVueDevToolsOptions {
+  /**
+   * append an import to the module id ending with `appendTo` instead of adding a script into body
+   * useful for projects that do not use html file as an entry
+   *
+   * WARNING: only set this if you know exactly what it does.
+   * @default ''
+   */
+  appendTo?: string | RegExp
+
+  /**
+   * Customize openInEditor host (e.g. http://localhost:3000)
+   * @default false
+   */
+  openInEditorHost?: string | false
+
+  /**
+   * DevTools client host (e.g. http://localhost:3000)
+   * useful for projects that use a reverse proxy
+   * @default false
+   */
+  clientHost?: string | false
+}
+const options: VitePluginVueDevToolsOptions = {
+  appendTo: '',
+  openInEditorHost: false,
+  clientHost: false
+}
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-  ],
+  plugins: [vue(), vueJsx(), VueDevTools(options)],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
